9d4756037f1d3dacf077458b286b8501b5d7a93a
RonaldoRodrigues <ronlima1@live.com>
2015-08-20 23:48:28 -0300

    deletando arquivos

--- views/index/index.php
+++
@@ -1,9 +0,0 @@
-
-<section id="bem-vindo" class="centralizar">
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-      <label class="titulo-main">Turing</label>
-    </div>
-   </div> 
-</div>
--- views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-
-		  </div>
-		</div>
-		    <script src="<?php echo Config::pegar('URL'); ?>js/jquery.js"></script>
-            <script src="<?php echo Config::pegar('URL'); ?>js/app.js"></script>
-	</body>
-</html>
